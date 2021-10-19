381326f4cd171091ee3f68397d6dad582c81a341
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-19 19:19:45 +0530

    Connection has been created successfully

---
+++ EmpPayRoll/EmployeeRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmpPayRoll
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=DESKTOP-04NE37L;Initial Catalog=Payroll_Service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        
+        public void DBconnection()
+        {
+            connection.Open();
+            if(connection.State==ConnectionState.Open)
+            {
+                Console.WriteLine("Connection has been Created Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Connection is Not Established");
+            }
+            connection.Close();
+        }
+
+       
+    }
+}
--- EmpPayRoll/Program.cs
+++ EmpPayRoll/Program.cs
@@ -7,6 +7,8 @@ namespace EmpPayRoll
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE EMPLOYEE PAY ROLL PROGRAM!");
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            employeeRepo.DBconnection();
         }
     }
 }
