e7db3298b459c66b64a5a589574b45ac4583c60c
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-19 17:33:13 +0530

    Welcome message addded

---
+++ EmpPayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpPayRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO THE EMPLOYEE PAY ROLL PROGRAM!");
+        }
+    }
+}
